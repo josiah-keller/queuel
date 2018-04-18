58ffe24a6d03a8fb2a7aa9f4f969d22eab60b5e7
Josiah Keller <josiahbkeller@gmail.com>
2018-04-17 21:57:48 -0400

    Properly update the statusIndex in queue status component
    Fixes #19

--- src/app/admin/queue-status/queue-status.component.ts
+++ src/app/admin/queue-status/queue-status.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-queue-status',
   templateUrl: './queue-status.component.html',
   styleUrls: ['./queue-status.component.scss']
 })
-export class QueueStatusComponent implements OnInit {
+export class QueueStatusComponent implements OnChanges {
   @Input() status : string;
   @Output() onStatusChange : EventEmitter<string> = new EventEmitter<string>();
   
@@ -16,7 +16,7 @@ export class QueueStatusComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
     this.statusIndex = this.statuses.indexOf(this.status);
   }
 
