17c53b29b1c08a57a7eb7a642560701136510edb
Josiah Keller <josiahbkeller@gmail.com>
2018-04-19 22:04:55 -0400

    Refactoring to deal with lazy-loading issue in Angular 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminComponent } from "./admin/admin/admin.component";
 import { DisplayComponent } from "./display/display/display.component";
 import { AuthGuard } from './auth.guard';
 
 const routes : Routes = [
   {
     path: "admin",
-    component: AdminComponent,
     loadChildren: "app/admin/admin.module#AdminModule",
   },
   {
